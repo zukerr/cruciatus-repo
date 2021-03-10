6e2142c2d0ee11deb9fdb38a34a8d16cf7fed088
zukerr <44325196+zukerr@users.noreply.github.com>
2021-03-10 01:12:06 +0100

    dungeon tuning
    -getting hit by pummeler's morning star gives player immunity for getting hit again in 1s
    -all basic mobs now give more count
    -enemies layout in dungeon2 changed

--- Cruciatus/Assets/Scripts/Enemies/Bosses/Pummeler/PummelerMorningStar.cs
+++ Cruciatus/Assets/Scripts/Enemies/Bosses/Pummeler/PummelerMorningStar.cs
@@ -6,17 +6,32 @@ public class PummelerMorningStar : MonoBehaviour
 {
     [SerializeField]
     private float morningStarDamage = 50f;
+    [SerializeField]
+    private float immunityTime = 1f;
+
+    private static bool playerHasImmunity = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<PlayerCharacter>() != null)
         {
+            if(playerHasImmunity)
+            {
+                return;
+            }
             PlayerCharacter.instance.DamagablePlayer.ModifyHealth(-morningStarDamage, DamageType.Physical);
+            playerHasImmunity = true;
+            Invoke(nameof(TurnOffPlayerImmunity), immunityTime);
         }
     }
 
     public void SetDamage(float value)
     {
         morningStarDamage = value;
+    }
+
+    private void TurnOffPlayerImmunity()
+    {
+        playerHasImmunity = false;
     }
 }
